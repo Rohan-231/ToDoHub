a4ca688f92f03d628f204d75501c6c441d2e6d61
Rohan Mistry <94956818+Rohan-231@users.noreply.github.com>
2024-11-10 21:52:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, useActionData } from "react-router-dom";
 import "./App.css";
 
-// pages
-<<<<<<< HEAD
-// import Projects from "./pages/Projects/";
-// import PageNotFound from "./pages/PageNotFound/";
-// import ProjectDetails from "./pages/ProjectDetails/";
 
-=======
->>>>>>> 9bbcaf8684bbd40e0e831a69c329297655842a71
 import Task from "./components/task";
 
 const App = () => {
