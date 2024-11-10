f61c5ee4ae6c43faeab0a9557ea0907f06436ff6
Rohan Mistry <94956818+Rohan-231@users.noreply.github.com>
2024-11-10 20:47:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,7 @@ import { BrowserRouter as Router, Routes, Route, useActionData } from "react-rou
 import "./App.css";
 
 // pages
-import Projects from "./pages/Projects/";
-import PageNotFound from "./pages/PageNotFound/";
-import ProjectDetails from "./pages/ProjectDetails/";
 import Task from "./components/task";
-import SearchBar from "./components/searchbar";
 
 const App = () => {
 
